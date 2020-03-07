cb2457b6122339d7531d2a1661879c888644b843
Christopher Leja <christopher.leja@gmail.com>
2020-03-07 22:32:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Chris"
+  end
+  
+  get '/hometown' do
+    "My hometown is Fort Collins, Colorado"
+  end
+  
+  get '/favorite-song'
+    "My favorite song is 'I Wish I Was Sober' by Frightened Rabbit"
+  end
+  
 end
